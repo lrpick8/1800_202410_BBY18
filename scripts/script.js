27d8a14dbdb37ebe018d649d50fa32db8e284206
armaa <asidhu233@my.bcit.ca>
2024-11-02 15:47:07 -0700

    Added firebase, removed an extra find a match button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
+
 function sayHello() {
     
 }
